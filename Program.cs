8ae4910aed84a7495a3873433bdadbf60fd109bb
Sergio-Lopezz <sergio.2385z@gmail.com>
2021-11-26 10:38:38 -0500

    Primer commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsolaIMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
